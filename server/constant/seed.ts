c4e7543963e36ea6937962fb8b0599ccc4a71a31
makano <makanobush@gmail.com>
2024-05-04 11:13:14 +0300

    Integration of IovieDDL: -1002%

--- server/constant/seed.ts
+++ server/constant/seed.ts
@@ -1,6 +1,6 @@
-import { Noise } from 'noisejs'; 
 import seedrandom from 'seedrandom';
 import { worldData } from './world.js';
+import { Noise } from '../lib/noise/index.js';
 
 export const seedrng = seedrandom(worldData.seed);
 export const noise = new Noise(seedrng());
